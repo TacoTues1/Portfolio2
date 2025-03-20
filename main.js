8d5f93f67f61a6ec2a981830d33f4c4d483a8175
alfonzperez <alfonzperez92@gmail.com>
2025-03-20 15:59:14 +0800

    major adjustment, contact added make more responsive add more features

--- main.js
+++ main.js
@@ -25,3 +25,19 @@ function typeWriter() {
 }
 
 window.onload = typeWriter;
+
+window.addEventListener('scroll', function () {
+    const backToTopButton = document.getElementById('backToTop');
+    if (window.scrollY > 200) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+}
